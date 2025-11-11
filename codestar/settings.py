ebc5ad12a5af2a0a40738f9730d2d9e7f076d6c6
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-11 15:14:22 +0000

    added to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$5!a%si6b!ctx9&(-2t(6won^3gl6resbjoiv-#v28b4#+w#m&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
