014a5e7fd9c6bfa6342b7d952ac021fd11de2c60
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-11 16:06:24 +0000

    added server

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$5!a%si6b!ctx9&(-2t(6won^3gl6resbjoiv-#v28b4#+w#m&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
