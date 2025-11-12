085e8f5564260db93fd985df9ce4790b63ac35c1
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-12 15:15:41 +0000

    update settings

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_Hby4YAFRw1Xd@ep-young-shape-agcma90e.c-2.eu-central-1.aws.neon.tech/grave_blog_hedge_175145")
-os.environ.setdefault("SECRET_KEY", "140410")
+os.environ.setdefault("SECRET_KEY", "100514")
