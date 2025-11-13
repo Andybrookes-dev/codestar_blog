32845babb69c51c75811e665403b215d79804dc3
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-13 16:23:36 +0000

    added new view

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
+
+    
