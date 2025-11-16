0976e0192faae7ede942934a07ac61183e4e68a3
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-16 16:44:13 +0000

    added js file

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECRET_KEY = 'django-insecure-$5!a%si6b!ctx9&(-2t(6won^3gl6resbjoiv-#v28b4#+w#m&'
 SECRET_KEY = os.environ.get("SECRET_KEY")   
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True  
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
