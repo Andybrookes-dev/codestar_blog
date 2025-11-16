5d18bf502dbdb9ae6e591decca7fb288cb1a9707
AndyMusicproducer <andybrookes78@icloud.com>
2025-11-16 16:30:04 +0000

    updated views

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -25,7 +23,6 @@ class Post(models.Model):
         return f"{self.title} | written by {self.author}"
   
 
-
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -33,12 +30,10 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)  
-    new_field = models.SlugField()  
-
+    created_on = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-         
